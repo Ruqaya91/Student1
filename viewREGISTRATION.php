93b04f407f71cb3ba4f7af93febbfc1e7c196b03
Ruqaya91 <ruqayagilal1@gmail.com>
2025-06-29 23:19:09 +0800

    Update viewREGISTRATION.php

--- viewREGISTRATION.php
+++ viewREGISTRATION.php
@@ -1,49 +1,25 @@
-<!DOCTYPE html>
-<html><!-- htmls starts -->
+<?php
+$dataFile = 'data.txt';
 
-    <head>
-        <title>School Registrations</title>
-    </head>
+if (file_exists($dataFile)) {
+    $data = file_get_contents($dataFile);
+    $users = explode("\n", trim($data));
 
-    <body> <!-- body starts -->
-        <h1>School Registration Page</h1>
+    echo "<h1>Registered Users</h1>";
 
-        <?php //php starts
-
-        // Checks if the data.txt file exists
-        if (file_exists('data.txt')) 
-        {
-            
-            //explode() function breaks strings into an array
-            // this splits the contents of the lines from the data.txt file
-            // the "\n" is the delimeter so each new line in the array will be split in the data.txt file
-            $lines = explode("\n", file_get_contents('data.txt'));
-
-            foreach ($lines as $line) 
-            {
-                //if the line in the data.txt file isnt empty it's gonna be further split by the " | " delimiter that is in each line
-                if (!empty($line)) 
-                {
-                    // Split the line by delimiter " | "
-                    $userInfo = explode(" | ", $line);
-
-                    // displays the data the user inputs on the registration form by getting each index of the userinfo array
-                    // <strong> this tag makes text bold/distinct from the other text
-                    echo "<p> <strong>Full Name:</strong> " . htmlspecialchars($userInfo[0]) . "</p>";
-                    echo "<p> <strong>Email:</strong> " . htmlspecialchars($userInfo[1]) . "</p>";
-                    echo "<p> <strong>Phone Number:</strong> " . htmlspecialchars($userInfo[2]) . "</p>";
-                    echo "<p> <strong>Profile Picture:</strong> <img src='" . htmlspecialchars($userInfo[3]) . "' alt='Profile Picture' style='width:100px;'></p>";
-                    echo "<p> <strong>Transcript:</strong> <a href='" . htmlspecialchars($userInfo[4]) . "' download>Click to download transcript</a></p>";
-                }
-            }
+    foreach ($users as $user) {
+        if (!empty($user)) {
+            list($fullName, $email, $phone, $profilePicPath, $transcriptPath) = explode(' | ', $user);
+            echo "<div style='border: 1px solid #ccc; padding: 10px; margin: 10px;'>";
+            echo "<p><strong>Full Name:</strong> $fullName</p>";
+            echo "<p><strong>Email:</strong> $email</p>";
+            echo "<p><strong>Phone:</strong> $phone</p>";
+            echo "<p><strong>Profile Picture:</strong><br><img src='$profilePicPath' alt='Profile Picture' style='width: 100px;'></p>";
+            echo "<p><strong>Transcript:</strong> <a href='$transcriptPath' download>Download Transcript</a></p>";
+            echo "</div>";
         }
-        //if the data.txt file doesn't exist there was something wrong in submission
-        else 
-        {
-            echo "<br>There is no data in the registration form, please go back and submit the form with all of your information put in.";
-        }
-        ?> <!-- php ends-->
-
-    </body> <!-- body ends -->
-
-</html> <!-- html ends -->
+    }
+} else {
+    echo "<p>No registrations found.</p>";
+}
+?>
