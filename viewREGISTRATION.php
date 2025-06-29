c7b7c0c18efd7ed7d0939bb49efb7462a005911a
Ruqaya91 <ruqayagilal1@gmail.com>
2025-06-29 23:48:38 +0800

    Update viewREGISTRATION.php

--- viewREGISTRATION.php
+++ viewREGISTRATION.php
@@ -1,12 +1,36 @@
+
+<head>
+    <title>View Registrations</title>
+</head>
+
 <?php
-	$data = file_get_contents('data.txt.');
+echo "<h2><center>--User Registrations--</center></h2>"; // title
 
-	//displaying all the fields that user inpuit
-	echo "Full Name = ". $_POST["fullName"];
-	echo "<br/>";
-	echo "Email = ". $_POST["email"];
-	echo "<br/>";
-	echo "Phone Number = ". $_POST["phoneNumber"];
-	echo "<br/>";
 
+// grab the data from data.txt 
+$file = 'data.txt';
+if (file_exists($file)) {
+    $data = file_get_contents($file);
+    $lines = explode("\n", trim($data));
+
+// create a registration list 
+    foreach ($lines as $line) {
+        list($fullName, $email, $phoneNumber, $pfp_path, $transcript_path) = explode(" | ", $line);  // explode breaks string into array, in this case it breaks it by every instance of | (future note) 
+
+        echo "<div>";
+        echo "<p><strong>Full Name:</strong> $fullName</p>";
+		
+        echo "<p><strong>Email:</strong> $email</p>";
+		
+        echo "<p><strong>Phone Number:</strong> $phoneNumber</p>";
+		
+        echo "<p><strong>Profile Picture:</strong> <img src='$pfp_path' width='100' height='100'></p>";
+		
+        echo "<p><strong>Transcript:</strong> <a href='$transcript_path' download>Download Transcript</a></p>";
+		
+        echo "</div><hr>";
+    }
+} else {
+    echo "No registrations found.";   // if theres no data
+}
 ?>
